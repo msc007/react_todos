255ddefb0ea9ba4274bb2d4a21b52ec2d291c006
msc007 <alstjr5569@gmail.com>
2019-01-14 17:00:14 -0800

    pass states to todo component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,44 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Todos from './components/Todos';
+
 import './App.css';
 
+/*
+ * Main application
+ *
+ */
 class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Take out the trash',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Dinner with wife',
+        complted: false
+      },
+      {
+        id: 3,
+        title:'Meeting with boss',
+        completed: false
+      }
+    ]
+  }
+
+
   render() {
+    //console.log(this.state.todos);
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {/*passing states to todos component*/} 
+        <Todos todos={this.state.todos}/>
       </div>
     );
   }
 }
 
 export default App;
+ 
---
+++ src/components/Todos.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Todos extends Component {
+  render() {
+    console.log(this.props.todos)
+
+    return (
+      <div>
+      <h1>Todos</h1>
+      </div>
+    );
+  }
+}
+
+export default Todos;
+ 
